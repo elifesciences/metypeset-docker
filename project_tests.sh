14419fca2d81904ba5bdf8ba6c87107d66775031
Daniel Ecer <de-code@users.noreply.github.com>
2018-07-26 09:25:17 +0100

    run project tests (#4)

--- project_tests.sh
+++ project_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker run --rm elifesciences/metypeset /bin/bash -c \
+docker run --rm elifesciences/metypeset:${IMAGE_TAG} /bin/bash -c \
   'pip install -r requirements.dev.txt && pytest'
